abc8cf739c922fb27dc7ddf4c20f521fb9b17e0d
kraulain <kraulain@gmail.com>
2023-07-25 20:09:21 +0100

    Add merge sort function

--- src/core.clj
+++ src/core.clj
@@ -228,3 +228,18 @@
 
 ;; Merge sort implementation
 
+(defn merge [l1 l2]
+  (lazy-seq
+   (cond
+     (empty? l1)
+     l2
+
+     (empty? l2)
+     l1
+
+     (< (first l1) (first l2))
+     (cons (first l1) (merge (rest l1) l2))
+
+     :else
+     (cons (first l2) (merge l1 (rest l2))))))
+
